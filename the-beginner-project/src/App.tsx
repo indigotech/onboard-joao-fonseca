010760ae654ec68caf0d56c9ef00e0a3c09ec5b6
joaopfa <joaopfa@hotmail.com>
2020-09-02 12:43:06 -0300

    primeiras alterações para a login screen pedida na task. A ser terminada depois do almoço

--- the-beginner-project/src/App.tsx
+++ the-beginner-project/src/App.tsx
@@ -4,10 +4,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-logo">Hello World</h1>
-      </header>
+    <div className="Container">
+      <div className="Device-container">
+        <h1 className="Title">Bem-vindo(a) à Taqtile!</h1>
+        <form className="Form-login">
+          <label>E-mail</label>
+          <input
+          className="Input"
+            type="email"
+          />
+          <label>Senha</label>
+          <input
+          className="Input"
+          type="password"
+          />
+          <button
+          type="submit"
+          className="Button"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
