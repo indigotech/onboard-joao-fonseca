b9464ea96da5ae7126597a3af84433cabfc5dfcb
joaopfa <joaopfa@hotmail.com>
2020-09-02 15:23:05 -0300

    adicionando validação aos inputs

--- the-beginner-project/src/App.tsx
+++ the-beginner-project/src/App.tsx
@@ -8,15 +8,26 @@ function App() {
       <div className="Device-container">
         <h1 className="Title">Bem-vindo(a) à Taqtile!</h1>
         <form className="Form-login">
-          <label>E-mail</label>
-          <input
+          <label htmlFor="email">E-mail</label>
+          <input 
+          required
           className="Input"
-            type="email"
+          type="email"
+          placeholder="email@email.co"
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+          title="Not a valid email format"
+          id="email"
           />
-          <label>Senha</label>
-          <input
+          <label htmlFor="password">Senha</label>
+          <input 
+          required
           className="Input"
           type="password"
+          name="password"
+          pattern="(?=.*\d)(?=.*[a-z]).{7,}"
+          title="7 characters minimum, and should have at least one digit and one letter"
+          placeholder="7 characters minimum"
+          id="password"
           />
           <button
           type="submit"
