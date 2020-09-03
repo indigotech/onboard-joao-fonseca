62536824d4e50271b9cb40db3759550da46dc6e4
joaopfa <joaopfa@hotmail.com>
2020-09-03 20:32:08 -0300

    trazendo dados do server, agora com dificuldades para controlar os inputs e associar o valor que recebem a um estado que tenha seu valor passado para o body da requisição

--- the-beginner-project/src/Components/LoginPage/LoginPage.tsx
+++ the-beginner-project/src/Components/LoginPage/LoginPage.tsx
@@ -1,44 +1,59 @@
 import React from 'react';
 import './styled.css';
-//import {client} from './QueryGQL'
+//import { setToken } from '../../Services/token'
+import {client} from '../../Services/QueryGQL'
+//import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+function LoginPage() {
+
+//   const handleInputChange = (event: Event) => {
+//     const { name, value }: { name: string; value: string} = event.target
+//     setState({ [name]: value })
+// }
+
+  const handleSubmit = () => {
+  console.log(client)
+  console.log("oi")
+}
 
 
-
-function App() {
   return (
-      <div className="Login-container">
-        <h1 className="Title">Bem-vindo(a) à Taqtile!</h1>
-        <form className="Form-login">
-          <label htmlFor="email">E-mail</label>
-          <input 
-          required
-          className="Input"
-          type="email"
-          placeholder="email@email.co"
-          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-          title="Not a valid email format"
-          id="email"
-          />
-          <label htmlFor="password">Senha</label>
-          <input 
-          required
-          className="Input"
-          type="password"
-          name="password"
-          pattern="(?=.*\d)(?=.*[a-z]).{7,}"
-          title="7 characters minimum, and should have at least one digit and one letter"
-          placeholder="7 characters minimum"
-          id="password"
-          />
-          <button
-          type="submit"
-          className="Button"
-          >
-            Entrar
-          </button>
-        </form>
-      </div>
+    <div className="Login-container">
+    <h1 className="Title">Bem-vindo(a) à Taqtile!</h1>
+    <form
+    onSubmit={()=>handleSubmit()}
+    className="Form-login"
+    >
+        <label htmlFor="email">E-mail</label>
+        <input 
+        required
+        className="Input"
+        type="email"
+        placeholder="email@email.co"
+        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+        title="Not a valid email format"
+        id="email"
+        />
+        <label htmlFor="password">Senha</label>
+        <input 
+        required
+        className="Input"
+        type="password"
+        name="password"
+        pattern="(?=.*\d)(?=.*[a-z]).{7,}"
+        title="7 characters minimum, and should have at least one digit and one letter"
+        placeholder="7 characters minimum"
+        id="password"
+        />
+        <button
+        type="submit"
+        className="Button"
+        >
+        Entrar
+        </button>
+    </form>
+    </div>
   );
 }
 
-export default App;
+export default LoginPage;
