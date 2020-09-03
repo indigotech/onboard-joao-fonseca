5951ca336c1af39118dba2178b2c67b4384f5945
joaopfa <joaopfa@hotmail.com>
2020-09-03 15:53:41 -0300

    organizando login page numa pasta components, e começando a implementar a integraçao com o graphQL

---
+++ the-beginner-project/src/Components/LoginPage/LoginPage.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './styled.css';
+//import {client} from './QueryGQL'
+
+
+
+function App() {
+  return (
+      <div className="Login-container">
+        <h1 className="Title">Bem-vindo(a) à Taqtile!</h1>
+        <form className="Form-login">
+          <label htmlFor="email">E-mail</label>
+          <input 
+          required
+          className="Input"
+          type="email"
+          placeholder="email@email.co"
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+          title="Not a valid email format"
+          id="email"
+          />
+          <label htmlFor="password">Senha</label>
+          <input 
+          required
+          className="Input"
+          type="password"
+          name="password"
+          pattern="(?=.*\d)(?=.*[a-z]).{7,}"
+          title="7 characters minimum, and should have at least one digit and one letter"
+          placeholder="7 characters minimum"
+          id="password"
+          />
+          <button
+          type="submit"
+          className="Button"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+  );
+}
+
+export default App;
